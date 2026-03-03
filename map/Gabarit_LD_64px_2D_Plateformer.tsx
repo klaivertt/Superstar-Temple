e9d9ff9715b3ca3c0946e118e653671fc6b294f7
Lexo <151062940+Lexo20@users.noreply.github.com>
2026-03-03 10:18:56 +0100

    add tiled map
    a toi de jouer mon Guillou

---
+++ map/Gabarit_LD_64px_2D_Plateformer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Gabarit_LD_64px_2D_Plateformer" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="Gabarit_LD_64px_2D_Plateformer.png" width="512" height="512"/>
+</tileset>
